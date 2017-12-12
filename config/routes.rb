f9662dbd660e7497f8811eb58e466c0f7909fbc0
Jason Yeomans <jyeo098@gmail.com>
2017-12-11 22:34:13 -0500

    fix root index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   devise_scope :user do
     authenticated :user do
-      root 'gymnasia#show', as: :authenticated_root
+      root 'gymnasia#index', as: :authenticated_root
     end
   
     unauthenticated do
