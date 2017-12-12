4b3448dbf5873befb43dc2fa1d0a71a92b38c485
Patrick Eng <peng@gmu.edu>
2017-12-12 02:11:24 +0000

    Added Authenticated Redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   
   devise_scope :user do
-  authenticated :user do
-    root 'home#index', as: :authenticated_root
-  end
-
-  unauthenticated do
-    root 'devise/sessions#new', as: :unauthenticated_root
-  end
+    authenticated :user do
+      root 'gymnasia#show', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
 end
 
   resources :gymnasia
